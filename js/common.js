d74923c9715c4951055c8cfc0eef1fa02f4240b8
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2018-12-23 15:26:17 +0300

    23.12

--- js/common.js
+++ js/common.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 
-
+	$('#menuTrigger').click(function(){
+		$('#popup__background').toggle();
+		$('.popup__menu').toggle();
+		$(this).toggleClass('active');
+	});
 
 
 });
